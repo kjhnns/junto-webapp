325a380499269f2564efc7dc430333228ff5d2f1
Johannes Klumpe <johannes.klumpe@gmail.com>
2020-04-14 23:27:50 +0200

    feat: adds card design

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -5,6 +5,8 @@ import { Flex } from '@components/Grid'
 import styled, { themeGet } from '@style'
 import { isLoggedIn, signOut, getUser } from '../../util/auth'
 import Wrapper from './Wrapper'
+import checked from './checked.svg'
+import unchecked from './unchecked.svg'
 
 const Card = styled.div`
   display: flex;
@@ -26,6 +28,17 @@ const Motivation = styled.p`
   color: ${themeGet('colors.black')};
   margin: 0;
 `
+const Follower = styled.p`
+  font-size: ${themeGet('fontSizes.2')};
+  color: ${themeGet('colors.black')};
+  margin: 0;
+`
+
+const CheckMarkContainer = styled.div`
+  width: 32px;
+  height: 32px;
+  margin: 0 2em;
+`
 
 const Dashboard = () => {
   const [loggedIn, setLoggedIn] = useState(false)
@@ -41,9 +54,42 @@ const Dashboard = () => {
     <Wrapper>
       <Text as="h1">Your Habits</Text>
       <Card>
-        <Flex flexDirection="column">
-          <Title>Run or Exercise</Title>
-          <Motivation>to live a healthier life</Motivation>
+        <Flex
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+          flex="1"
+        >
+          <Flex flexDirection="column" flex="1">
+            <Title>Run or Exercise</Title>
+            <Motivation>to live a healthier life</Motivation>
+          </Flex>
+          <Follower>
+            <strong>4</strong> Followers
+          </Follower>
+          <CheckMarkContainer>
+            <img width="32px" height="32px" src={checked} alt="" />
+          </CheckMarkContainer>
+        </Flex>
+      </Card>
+
+      <Card>
+        <Flex
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+          flex="1"
+        >
+          <Flex flexDirection="column" flex="1">
+            <Title>Run or Exercise</Title>
+            <Motivation>to live a healthier life</Motivation>
+          </Flex>
+          <Follower>
+            <strong>4</strong> Followers
+          </Follower>
+          <CheckMarkContainer>
+            <img width="32px" height="32px" src={unchecked} alt="" />
+          </CheckMarkContainer>
         </Flex>
       </Card>
       {loggedIn !== null ? `Hello ${getUser().displayName}` : ''}
