9eec70dfbabce0660eb2c970e0735b0bcf1af3bd
Johannes Klumpe <johannes.klumpe@gmail.com>
2020-04-18 22:17:11 +0200

    feat: adds a redirect for signed in users on index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,43 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { navigate } from 'gatsby'
 
 import { Layout } from '@components/Layout'
 import { SEO } from '@components/SEO'
 import { Box } from '@components/Grid'
 import { Heading, Text } from '@components/Typography'
 import { LoginAndSignupBanner } from '@components/LoginAndSignupBanner'
+import { isLoggedIn } from '@auth'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO />
-    <Box
-      sx={{
-        p: 4,
-        pb: '84px',
-        minHeight: '100vh',
-        bg: 'gray.100',
-        display: 'grid',
-        alignItems: 'center',
-      }}
-    >
-      <Box maxWidth="500px">
-        <Heading as="h1" fontSize={8}>
-          junto
-        </Heading>
-        <Text mb={3} fontSize={4} color="gray.600">
-          social habit tracking
-        </Text>
-        <Text fontSize={3} lineHeight="1.4">
-          Good resolutions don’t work — social mechanisms do. Junto levarages
-          behavioral science to help users achieve their personal goals by
-          building good habits and breaking bad habits —{' '}
-          <Text display="inline" fontWeight="bold">
-            together.
+const IndexPage = () => {
+  useEffect(() => {
+    async function loggedInRedirect() {
+      if (await isLoggedIn()) {
+        navigate('/dashboard', { replace: true })
+      }
+    }
+    loggedInRedirect()
+  })
+
+  return (
+    <Layout>
+      <SEO />
+      <Box
+        sx={{
+          p: 4,
+          pb: '84px',
+          minHeight: '100vh',
+          bg: 'gray.100',
+          display: 'grid',
+          alignItems: 'center',
+        }}
+      >
+        <Box maxWidth="500px">
+          <Heading as="h1" fontSize={8}>
+            junto
+          </Heading>
+          <Text mb={3} fontSize={4} color="gray.600">
+            social habit tracking
           </Text>
-        </Text>
+          <Text fontSize={3} lineHeight="1.4">
+            Good resolutions don’t work — social mechanisms do. Junto levarages
+            behavioral science to help users achieve their personal goals by
+            building good habits and breaking bad habits —{' '}
+            <Text display="inline" fontWeight="bold">
+              together.
+            </Text>
+          </Text>
+        </Box>
       </Box>
-    </Box>
-    <LoginAndSignupBanner />
-  </Layout>
-)
+      <LoginAndSignupBanner />
+    </Layout>
+  )
+}
 
 export default IndexPage
