36de1c93970866dae256d6e8b74f1a243528d408
Johannes Klumpe <johannes.klumpe@gmail.com>
2020-05-12 11:20:11 +0200

    fix: 2nd attempt at resolving refresh glitch

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,29 +1,20 @@
 import React from 'react'
 
 import { Link } from '@components/Link'
-import { Layout } from '@components/Layout'
 import { Box } from '@components/Grid'
 import { SEO } from '@components/SEO'
 import { Text } from '@components/Typography'
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404 — Not found" />
-    <Box
-      sx={{
-        minHeight: '100vh',
-        bg: 'gray.100',
-        display: 'grid',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Box sx={{ fontSize: 4 }}>
-        <Text mb={3}>You just hit a route that doesn&#39;t exist...</Text>
-        <Link to="/">Take me home.</Link>
-      </Box>
+    <Box sx={{ fontSize: 4, textAlign: 'center' }}>
+      <Text mb={3} mt={5}>
+        You just hit a route that doesn&#39;t exist...
+      </Text>
+      <Link to="/">Take me home.</Link>
     </Box>
-  </Layout>
+  </>
 )
 
 export default NotFoundPage
