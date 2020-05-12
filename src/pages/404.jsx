f72c14112d20b0666d74ce367466ef0d39d014fd
Johannes Klumpe <johannes.klumpe@gmail.com>
2020-05-12 12:05:24 +0200

    fix: attempt to fix style override issue 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
 
 import { Link } from '@components/Link'
-import { Layout } from '@components/Layout'
 import { Box } from '@components/Grid'
 import { SEO } from '@components/SEO'
 import { Text } from '@components/Typography'
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404 — Not found" />
-    <Box
-      sx={{
-        p: 4,
-        minHeight: '100vh',
-        bg: 'gray.100',
-        display: 'grid',
-        alignItems: 'center',
-      }}
-    >
-      <Box sx={{ fontSize: 4 }}>
-        <Text mb={3}>
-          You just hit a route that doesn&#39;t exist... the sadness.
-        </Text>
-        <Link to="/">Take me home.</Link>
+    <Box>
+      <Box
+        sx={{
+          p: 4,
+          minHeight: '100vh',
+          bg: 'gray.100',
+          display: 'grid',
+          alignItems: 'center',
+        }}
+      >
+        <Box sx={{ fontSize: 4 }}>
+          <Text mb={3}>
+            You just hit a route that doesn&#39;t exist... the sadness.
+          </Text>
+          <Link to="/">Take me home.</Link>
+        </Box>
       </Box>
     </Box>
-  </Layout>
+  </>
 )
 
 export default NotFoundPage
