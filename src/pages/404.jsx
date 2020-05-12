d43dbfcf981b83abf8179b508792a89511b2f71e
Johannes Klumpe <johannes.klumpe@gmail.com>
2020-05-12 11:40:25 +0200

    fix: 404 page style overrides

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 
 import { Link } from '@components/Link'
-import { Box } from '@components/Grid'
 import { SEO } from '@components/SEO'
 import { Text } from '@components/Typography'
+
+// ISSUE!
+// Somehow when reach router is not loaded and 404 is thrown (refreshing on habit/details page)
+// the 404 Page's style and layout overrides the pages that are then load by reach router
 
 const NotFoundPage = () => (
   <>
     <SEO title="404 — Not found" />
-    <Box sx={{ fontSize: 4, textAlign: 'center' }}>
-      <Text mb={3} mt={5}>
+    <>
+      <Text ml={5} mt={5}>
         You just hit a route that doesn&#39;t exist...
       </Text>
-      <Link to="/">Take me home.</Link>
-    </Box>
+      <Link ml={5} to="/">
+        Take me home.
+      </Link>
+    </>
   </>
 )
 
