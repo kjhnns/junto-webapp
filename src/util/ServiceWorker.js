9d2b340bbe10097532e1d977299fb5c1ef33ba1d
Johannes Klumpe <johannes.klumpe@gmail.com>
2020-09-18 10:58:51 +0200

    test: testing the active event in service worker

--- src/util/ServiceWorker.js
+++ src/util/ServiceWorker.js
@@ -1,3 +1,6 @@
 // show a notification after 15 seconds (the notification
 // permission must be granted first)
-console.log('hi i am the service worker')
+
+self.addEventListener('activate', event => {
+  console.log('Hi this is the service worker active state')
+})
