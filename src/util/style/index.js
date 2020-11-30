d8f2afdcfa28f0109ee3bed1c47ef5ad1fc9c790
Gregor Albrecht <hello@gregoralbrecht.com>
2020-11-30 15:44:16 +0100

    Make linter happy

--- src/util/style/index.js
+++ src/util/style/index.js
@@ -2,5 +2,5 @@ import propTypes from '@styled-system/prop-types'
 import { themeGet } from '@styled-system/theme-get'
 
 export { themeGet, propTypes }
-export * from 'styled-system'
-export default styled
+// export * from 'styled-system'
+// export default styled
