eb02dd8ed05d64c4be1848d0541cd79bb7931725
Johannes Klumpe <johannes.klumpe@gmail.com>
2020-11-30 22:24:14 +0100

    feat: better responsiveness for feedback fish

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ThemeProvider } from 'theme-ui'
-import { FeedbackFish } from '@feedback-fish/react'
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Global, css } from '@emotion/core'
 
-import { Button } from '@components/Button'
-import { Box } from '@components/Grid'
-
 import theme from '../../gatsby-plugin-theme-ui'
 import 'typeface-inter'
-
-// const FeedbackButton = () => (
-//   <B
-// )
 
 const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
@@ -32,11 +24,6 @@ const Layout = ({ children }) => (
       `}
     />
     <>{children}</>
-    <FeedbackFish projectId="ecbda4963f3b1f">
-      <Box sx={{ position: `absolute`, bottom: [`72px`, 3], right: [3] }}>
-        <Button>Give us feedback</Button>
-      </Box>
-    </FeedbackFish>
   </ThemeProvider>
 )
 
