873a81f6f8def5341ea5922d3c717e58791b2ca3
Gregor Albrecht <hello@gregoralbrecht.com>
2020-11-30 15:41:08 +0100

    feat: Integrate Feedback Fish

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Router } from '@reach/router'
-import { Layout } from '@components/Layout'
 import { SEO } from '@components/SEO'
 import { Dashboard } from '@components/Dashboard'
 import { CreateNewHabit } from '@components/CreateNewHabit'
@@ -11,7 +10,7 @@ import { HabitDetails } from '@components/HabitDetails'
 import { PrivateRoute } from '@components/PrivateRoute'
 
 const DashboardPage = () => (
-  <Layout>
+  <>
     <SEO />
     <Router>
       <PrivateRoute path="/dashboard" component={Dashboard} />
@@ -22,7 +21,7 @@ const DashboardPage = () => (
         component={HabitDetails}
       />
     </Router>
-  </Layout>
+  </>
 )
 
 export default DashboardPage
